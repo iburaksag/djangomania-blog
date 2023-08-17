b44748bcb0b4b424925b176a26234c89c523521c
Burak Sag <iburaksag@gmail.com>
2023-08-17 17:13:34 +0300

    Category detail page is created. User can see all the posts of a related category.

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import get_object_or_404, render, redirect
 
 from .forms import CommentForm
-from .models import Post
+from .models import Post, Category
 
-def detail(request,slug):
+def detail(request,category_slug, slug):
     post = get_object_or_404(Post, slug=slug)
 
     if request.method == 'POST':
         form = CommentForm(request.POST)
-        
+
         if form.is_valid():
             comment = form.save(commit=False)
             comment.post = post
@@ -20,3 +20,10 @@ def detail(request,slug):
 
     context = { 'post': post, 'form': form, }
     return render(request, 'blog/detail.html', context)
+
+def category(request,slug):
+    category = get_object_or_404(Category, slug=slug)
+    context = dict(
+        category = category,
+    )
+    return render(request, 'blog/category.html', context)
