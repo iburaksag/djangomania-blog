512d3bc033b44fd61a41a5209847c220714de5e1
Burak Sag <iburaksag@gmail.com>
2023-08-17 17:03:16 +0300

    Category model is created. Now, all posts have a category.

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,19 @@
 from django.db import models
 
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    class Meta:
+        ordering = ('title', )
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.title
+    
 
 class Post(models.Model):
+    category = models.ForeignKey(Category, related_name="posts", on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     intro = models.TextField()
@@ -11,6 +23,10 @@ class Post(models.Model):
     class Meta:
         ordering = ('-created_at',)
 
+    def __str__(self):
+        return self.title
+    
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)         
     name = models.CharField(max_length=255)    
@@ -18,4 +34,3 @@ class Comment(models.Model):
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
-    
